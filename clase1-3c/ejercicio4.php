39b2a6740a806aa5227ad30f1baffa408e7dc33f
SantiGO055 <santigonzalez05@gmail.com>
2020-08-26 11:43:41 -0300

    .

--- clase1-3c/ejercicio4.php
+++ clase1-3c/ejercicio4.php
@@ -1,5 +1,40 @@
 <?php
 
+$operador = array('+','-','*','/');
+$op1 = 5;
+$op2 = 8;
 
+echo "<br/>";
+foreach ($operador as $key => $value) {
+    switch ($value) {
+        
+        case '+':
+            echo "$op1 + $op2: ";
+            echo $op1 + $op2;
+            echo "<br/>";
+            break;
+        case '-':
+            echo "$op1 - $op2: ";
+            echo $op1 - $op2;
+            echo "<br/>";
+            break;
+        case '*':
+            echo "$op1 * $op2: ";
+            echo $op1 * $op2;
+            echo "<br/>";
+            break;
+        case '/':
+            if ($op2 == 0){
+                echo "No se puede dividir por 0";
+                echo "<br/>";
+            }
+            else{
+                echo "$op1 / $op2: ";
+                echo $op1 / $op2;
+                echo "<br/>";
+            }
+            break;
+    }
+}
 
 ?>
