0473f92b51124b35abc6e66c16130a23e269e8af
SantiGO055 <santigonzalez05@gmail.com>
2020-08-26 11:58:07 -0300

    Update ejercicio5.php

---
+++ clase1-3c/ejercicio5.php
@@ -0,0 +1,63 @@
+<?php
+
+$num = 50;
+
+if ($num == 20) {
+    echo "veinte ";
+}
+elseif ($num > 20 && $num<30) {
+    echo "veinti ";
+}
+elseif ($num == 30) {
+    echo "trinta";
+}
+elseif ($num >= 30 && $num < 40) {
+    echo "trinta y";
+}
+elseif ($num == 40) {
+    echo "cuarenta";
+}
+elseif ($num > 40 && $num < 50) {
+    echo "cuarenta y ";
+}
+elseif ($num == 50) {
+    echo "cincuenta";
+}
+elseif ($num>50 && $num < 60) {
+    echo "cincuenta y ";
+}
+elseif ($num == 60) {
+    echo "sesenta";
+}
+$unidad = substr($num, -1);
+switch ($unidad) {
+    case '1':
+        echo "uno";
+        break;
+    case '2':
+        echo "dos";
+        break;
+    case '3':
+        echo "tres";
+        break;
+    case '4':
+        echo "cuatro";
+        break;
+    case '5':
+        echo "cinco";
+        break;
+    case '6':
+        echo "seis";
+        break;
+    case '7':
+        echo "siete";
+        break;
+    case '8':
+        echo "ocho";
+        break;
+    case '9':
+        echo "nueve";
+        break;
+}
+
+?>
