1315922d611ab02f4c97d8926ed291eb5b945c5b
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2020-11-07 18:30:37 -0300

    .

---
+++ practicaSlimPHP/clases/usuario.php
@@ -0,0 +1,162 @@
+<?php
+namespace Clases;
+use Clases\Token;
+use Clases\Archivos;
+// include_once '../clasesParaParcial/archivos.php';
+
+
+class Usuario{
+    public $email;
+    public $clave;
+    public $imagenNombre;
+
+    public function __construct($email, $clave, $imagenNombre)
+    {
+        $this->setEmail($email);
+        $this->clave = $clave;
+        $this->imagenNombre = $imagenNombre;
+    }
+    
+    public function setEmail($email){
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $this->email = "emailNoValido";
+        }
+        else{
+            $this->email = $email;
+            
+        }
+    }
+
+    public static function Login($email,$clave){
+        $retorno = false;
+        $lista = Archivos::leerJson('users.json',$listaUsuarios);
+
+        
+        // var_dump($lista);
+        
+        if(isset($lista)){
+            foreach ($lista as $usuario) {
+                //$usuario['clave'] es la clave encriptada del json que levanto
+                if ($usuario['email'] == $email && Usuario::verificarContraseña($clave,$usuario['clave'])) {
+                    
+                    //Usuario::verificarContraseña($clave,$usuario['clave']);
+
+                    $token = Token::crearToken($usuario);
+                    
+                    return $token;
+                break;
+                }
+            }
+        }
+    }
+
+    public static function encriptarContraseña($clave){
+        return password_hash($clave, PASSWORD_DEFAULT);
+    }
+
+    public static function verificarContraseña($clave,$hash){
+        return password_verify($clave, $hash);
+    }
+
+    
+
+    public static function CrearUsuario($email,$claveEncriptada){
+        $retorno = false;
+        
+        if(!Usuario::buscarUsuario($email)){
+            echo $email;
+            $imagenNombre = Archivos::guardarImagen($_FILES,3670016,'./imagenes/',true);
+            
+            $usuario = new Usuario($email,$claveEncriptada,$imagenNombre);
+            if ($usuario->email != "emailNoValido") {
+                if(Archivos::guardarJson($usuario,'users.json')){
+                    $retorno = true;
+                }
+                if (isset($listaUsuarios)) {
+                    
+                    array_push($listaUsuarios);
+                }
+                else{
+                    
+                    $listaUsuarios = $usuario;
+                }
+            }
+            else{
+                $retorno = false;
+            }
+
+            
+        }
+        else
+        {
+            $retorno = false;
+        }
+        return $retorno;
+    }
+
+
+    public static function buscarUsuario($email)
+    {   
+        $retorno = false;
+        Archivos::leerJson('./users.json',$listaUsuarios);
+        //var_dump($listaUsuarios);
+
+        foreach ($listaUsuarios as $usuario) {
+            if ($usuario['email'] === $email) {
+                $retorno = true;
+            }
+            else{
+                $retorno = false;
+            }
+        }
+        return $retorno;
+        // if(isset( $listaUsuarios)){
+        //     if(Archivos::leerTxt('usuario.txt', $listaUsuarios))
+        //     {
+        //         foreach ($listaUsuarios as $auxUr)
+        //         {
+        //             if($email == $auxUr['email'])
+        //             {
+        //                 $retorno = true;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // return $retorno;
+        // }
+        
+    }
+    public static function asignarFotoNueva($email,$foto){
+        Archivos::leerJson('./users.json',$listaUsuarios);
+        //var_dump($listaUsuarios);
+        $nombreFoto = $_FILES["foto"]["name"];
+        for ($i=0; $i < count($listaUsuarios); $i++) { 
+            if ($listaUsuarios[$i]['email'] === $email) {
+                //var_dump($nombreFoto);
+                //$pathMover = "./imagenes/" . $listaUsuarios[$i]['imagenNombre'];
+
+                Archivos::moverImagen("./imagenes/imagen" . $listaUsuarios[$i]['imagenNombre'] , "./backup/".$listaUsuarios[$i]['imagenNombre']);
+                $usuarioAux = new Usuario($email,$listaUsuarios[$i]['clave'],$nombreFoto);
+                
+                Archivos::modificarJson("./users.json",$i,"imagenNombre",$nombreFoto);
+                $retorno = true;
+                return $retorno;
+            }
+            else{
+                $retorno = false;
+            }
+        }
+        // foreach ($listaUsuarios as $usuario) {
+            
+        // }
+
+        
+    }
+
+
+
+}
+
+
+?>
---
+++ practicaSlimPHP/src/controllers/UserController.php
@@ -0,0 +1,150 @@
+<?php
+namespace App\Controllers;
+use Clases\Usuario;
+// use App\Models\User;
+
+class UserController {
+    public function getAll ($request, $response, $args) {
+        // $rta = User::get();
+        // $rta = User::find(1);
+        // $rta = User::where('id', '>',  0)
+        // // ->where('campo', 'operador', 'valor')        
+        // ->get();
+
+        $response->getBody()->write(json_encode($rta));
+        return $response;
+    }
+
+    public function getOne($request, $response, $args)
+    {
+        $response->getBody()->write("getOne!");
+        return $response;
+    }
+    public function altaUsuario($request, $response, $args)
+    {
+        // echo $args['id'];
+        $parsedBody = $request->getParsedBody();
+        // var_dump( $parsedBody['email']);
+        // var_dump( $request);
+        // echo "<br>";
+        // echo $request['email'];
+        // echo "<br>";
+        // echo $request['email'];
+        // echo "hola";
+        $claveEncriptada = Usuario::encriptarContraseña($parsedBody['clave']);
+
+        if(Usuario::CrearUsuario($parsedBody['email'],$claveEncriptada))
+        {
+            $datos = 'Se creo el usuario correctamente!';
+        }
+        else
+        {
+            $datos = 'Error al crear usuario. Usuario existente';
+        }
+        $payload = json_encode($datos);
+
+        $response->getBody()->write($payload);
+        return $response
+          ->withHeader('Content-Type', 'application/json');
+    }
+    public function login($request, $response, $args)
+    {
+        // echo $args['id'];
+        $parsedBody = $request->getParsedBody();
+        // var_dump( $parsedBody['email']);
+        // var_dump( $request);
+        // echo "<br>";
+        // echo $request['email'];
+        // echo "<br>";
+        // echo $request['email'];
+        // echo "hola";
+        $token = Usuario::Login($parsedBody['email'], $parsedBody['clave']);
+
+        if(Usuario::Login($parsedBody['email'], $parsedBody['clave']))
+        {
+            $datos = 'Login Exitoso. TOKEN: ' . $token;
+        }
+        else
+        {
+            $datos = 'Nombre o Clave Incorrectas';                       
+        }
+        $payload = json_encode($datos);
+
+        $response->getBody()->write($payload);
+        return $response
+          ->withHeader('Content-Type', 'application/json');
+    }
+    public function Materia($request, $response, $args){
+        $parsedBody = $request->getParsedBody();
+        $token = $request->getHeader('token');
+        // $token = $header['token'];
+        $nombreMateria = $parsedBody['nombre'];
+        $cuatrimestre = $parsedBody['cuatrimestre'];
+        // $cuatrimestre = $_POST['cuatrimestre'] ?? "";
+        
+        $usuarioLogueado = Token::VerificarToken($token);
+        
+        if (!$usuarioLogueado) {
+            $datos = "Usuario no logueado, token incorrecto!";
+        }
+        else{
+            $materia = new Materias($nombreMateria,$cuatrimestre);
+            
+
+            if(Archivos::guardarJson($materia,'materias.json')){
+                $datos = "Materia guardada correctamente";
+            }
+            else{
+                $datos = "Ocurrio un error al guardar la materia";
+            }
+        }
+        $payload = json_encode($datos);
+
+        $response->getBody()->write($payload);
+        return $response
+          ->withHeader('Content-Type', 'application/json');
+    }
+    public function add($request, $response, $args)
+    {
+        $user = new User;
+        $user->name = "Juan";
+        $user->email = "Juan@mail.com";
+        $user->password = "sdxdsdsds";
+
+        $rta = $user->save();
+
+        $response->getBody()->write(json_encode($rta));
+        return $response;
+    }
+
+    public function update($request, $response, $args)
+    {
+        $id = $args['id'];
+        $user = User::find($id);
+
+        $user->name = "Peter";
+        $user->email = "nuevo@mail.com";
+
+        $rta = $user->save();
+
+        $response->getBody()->write(json_encode($rta));
+        return $response;
+    }
+
+    public function delete($request, $response, $args)
+    {
+        $id = $args['id'];
+        $user = User::find($id);
+
+        $rta = $user->delete();
+
+        $response->getBody()->write(json_encode($rta));
+        return $response;
+    }
+    // $respuesta = new stdClass;
+    // $respuesta->success = true;
+
+    // $respuesta->data = $datos; 
+
+    
+}
